a2bf3c653675cb11e32ba110a0eb8641d9c6388d
ajstewartlang <andrew.stewart@manchester.ac.uk>
2019-06-10 17:22:11 +0100

    first commit

---
+++ script_vis_3.R
@@ -0,0 +1,28 @@
+library(ggplot2)
+library(ggthemes)
+library(readr)
+library(dplyr)
+library(stringr)
+library(tools)
+
+full_trains <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-26/full_trains.csv")
+
+# Visualisation 3 ####
+full_trains %>% 
+  filter(str_detect(departure_station, "PARIS")) %>%
+  mutate(departure_station = toTitleCase(tolower(departure_station))) %>%
+  mutate(month = factor(month, levels = 1:12, labels = month.name)) %>%
+  group_by(month, year, departure_station) %>%
+  summarise(mean_num_of_canceled_trains = mean(num_of_canceled_trains, na.rm = TRUE)) %>%
+  ggplot(aes(x = departure_station, 
+             y = mean_num_of_canceled_trains,
+             colour = departure_station)) +
+  geom_jitter(width = .2, alpha = .6, size = 5, stroke = 1) +
+  guides(colour = FALSE) +
+  coord_flip() +
+  labs(x = NULL, y = NULL, 
+       title = "Cancelled Trains from Paris Railway Stations by Year",
+       subtitle = "Average Number of Cancellations per Month") +
+  theme(text = element_text(size = 12)) +
+  scale_color_tableau() +
+  facet_wrap(~ year) 
